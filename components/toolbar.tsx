88d7fe60e6a41ee6a6a7b45f96f1773053bf7cfd
mrtSahin <mrtsvs07@gmail.com>
2023-12-09 00:23:38 +0300

    toolbar devam

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -2,6 +2,11 @@
 
 import { Doc } from "@/convex/_generated/dataModel"
 import { IconPicker } from "./icon-picker"
+import { Button } from "./ui/button"
+import { ImageIcon, Smile, X } from "lucide-react"
+import { ElementRef, useRef, useState } from "react"
+import { useMutation } from "convex/react"
+import { api } from "@/convex/_generated/api"
 
 
 
@@ -15,6 +20,41 @@ export const Toolbar = ({
   initialData,
   preview
 }: ToolbarProps) => {
+
+  const inputRef = useRef<ElementRef<'textarea'>>(null)
+  const [isEditting, setIsEditting] = useState(false)
+  const [value, setValue] = useState(initialData.title)
+
+  const update = useMutation(api.documents.update)
+
+  const enableInput = () => {
+    if (preview) return
+
+    setIsEditting(true)
+    setTimeout(() => {
+      setValue(initialData.title)
+      inputRef.current?.focus()
+    }, 0)
+  }
+
+  const disableInput = () => setIsEditting(false)
+
+  const onInput = (value: string) => {
+    setValue(value)
+    update({
+      id: initialData._id,
+      title: value || "Untitled"
+    })
+  }
+  const onKeyDown = (
+    event: React.KeyboardEvent<HTMLTextAreaElement>
+  ) => {
+    if (event.key == "Enter") {
+      event.preventDefault()
+      disableInput()
+    }
+  }
+
   return (
     <div className="pl-[54px] group relative">
       {!!initialData.icon && !preview && (
@@ -24,9 +64,47 @@ export const Toolbar = ({
               {initialData.icon}
             </p>
           </IconPicker>
+          <Button
+            onClick={() => { }}
+            className="rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs"
+            variant='outline'
+            size='icon'
+          >
+            <X className="h-4 w-4" />
+          </Button>
         </div>
       )
       }
+      {!!initialData.icon && preview && (
+        <p className="text-6xl pt-6">
+          {initialData.icon}
+        </p>
+      )}
+      <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
+        {!initialData.icon && !preview && (
+          <IconPicker asChild onChange={() => { }}>
+            <Button // bastigimizda emoji pickeri aciyor
+              className="text-muted-foreground text-sm"
+              variant='outline'
+              size='sm'
+            >
+              <Smile className="h-4 w-4 mr-2" />
+              Add icon
+            </Button>
+          </IconPicker>
+        )}
+        {!initialData.coverImage && !preview && (
+          <Button
+            onClick={() => { }}
+            className="text-muted-foreground text-sm"
+            variant='outline'
+            size='sm'
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            Add cover
+          </Button>
+        )}
+      </div>
     </div >
   )
 } 
