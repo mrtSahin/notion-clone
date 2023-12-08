80defc0ce7bc9906d420785a53481d9299534f84
mrtSahin <mrtsvs07@gmail.com>
2023-12-08 23:06:43 +0300

    toolbar devam

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Doc } from "@/convex/_generated/dataModel"
+import { IconPicker } from "./icon-picker"
 
 
 
@@ -16,7 +17,16 @@ export const Toolbar = ({
 }: ToolbarProps) => {
   return (
     <div className="pl-[54px] group relative">
-      Toolbar
-    </div>
+      {!!initialData.icon && !preview && (
+        <div className="flex items-center gap-x-2 group/icon pt-6">
+          <IconPicker onChange={() => { }}>
+            <p className="text-6xl hover:opacity-75 transition">
+              {initialData.icon}
+            </p>
+          </IconPicker>
+        </div>
+      )
+      }
+    </div >
   )
 } 
