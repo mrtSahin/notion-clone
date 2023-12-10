d3a27d53a1817b0c8fff5f583c4d7bde51f60c6e
mrtSahin <mrtsvs07@gmail.com>
2023-12-10 14:16:31 +0300

    partialblock error fix

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,51 +1,48 @@
-"use client"
+"use client";
 
-
+import { useTheme } from "next-themes";
 import {
   BlockNoteEditor,
   PartialBlock
-} from '@blocknote/core'
-
+} from "@blocknote/core";
 import {
   BlockNoteView,
   useBlockNote
-} from '@blocknote/react'
+} from "@blocknote/react";
+import "@blocknote/core/style.css";
 
-import '@blocknote/core/style.css'
-import { useTheme } from 'next-themes'
-import { useEdgeStore } from '@/lib/edgestore'
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface EditorProps {
-  onChange: (valuew: string) => void
-  initialContent?: string
-  editable?: boolean
-}
+  onChange: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
+};
 
 const Editor = ({
   onChange,
   initialContent,
   editable
 }: EditorProps) => {
+  const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
 
-  const { resolvedTheme } = useTheme()
-  const {edgestore} = useEdgeStore()
-
-  const handleUpload = async(file:File)=>{ // editorde resim eklemek icin edge storela beraber kullaniyoruz
-    const response = await edgestore.publicFiles.upload({
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({ 
       file
-    })
+    });
 
-    return response.url
+    return response.url;
   }
 
   const editor: BlockNoteEditor = useBlockNote({
     editable,
-    initialContent:
-      initialContent
-        ? JSON.parse(initialContent) as PartialBlock[]
-        : undefined,
+    initialContent: 
+      initialContent 
+      ? JSON.parse(initialContent) as PartialBlock[] 
+      : undefined,
     onEditorContentChange: (editor) => {
-      onChange(JSON.stringify(editor.topLevelBlocks, null, 2))
+      onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
     },
     uploadFile: handleUpload
   })
@@ -54,10 +51,10 @@ const Editor = ({
     <div>
       <BlockNoteView
         editor={editor}
-        theme={resolvedTheme === 'dark' ? 'dark' : 'light'}
+        theme={resolvedTheme === "dark" ? "dark" : "light"}
       />
     </div>
   )
 }
 
-export default Editor
+export default Editor;
