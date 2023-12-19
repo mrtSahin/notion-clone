5384d46c04eec5b663505fe87e254e70c6766396
mrtSahin <mrtsvs07@gmail.com>
2023-12-19 19:33:11 +0300

    search-fix

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -68,9 +68,9 @@ export const SearchCommand = () => {
           {documents?.map((document) => (
             <CommandItem
               key={document._id}
-              value={`${document._id}`} // BURADA YANINA EKSTRA OLARAK DOSYA ISMINI DE EKLEDIGIMIZDEN DOLAYI USERINE BASINCA OYLE BIR DOCUMENTID OLMADIGI ICIN URL DE SIKINTI CIKYOR VE SAYFAYI ACAMIYOR HATA ATIYOR
+              value={`${document._id}-${document.title}`} // BURADA YANINA EKSTRA OLARAK DOSYA ISMINI DE EKLEDIGIMIZDEN DOLAYI USERINE BASINCA OYLE BIR DOCUMENTID OLMADIGI ICIN URL DE SIKINTI CIKYOR VE SAYFAYI ACAMIYOR HATA ATIYOR
               title={document.title}
-              onSelect={onSelect}
+              onSelect={()=>onSelect(document._id)} // üstteki sıkıntıyı gidermek için value değerini yollamak yerine sadece id yi yolluyoruz
             >
               {document.icon ? (
                 <p className='mr-2 text-[18px]'>
